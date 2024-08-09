159acd9c4cd9757011d09f88d3f9e47db1da336c
Apoorva Verma <apoorva.verma@oracle.com>
2024-08-09 19:16:55 +0530

    update

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,7 @@
 const escape = require('shell-quote').quote
 const isWin = process.platform === 'win32'
 
+
 module.exports = {
   '**/*.{js,jsx,ts,tsx,json,md,mdx,css,html,yml,yaml,scss,sass}': filenames => {
     const escapedFileNames = filenames
